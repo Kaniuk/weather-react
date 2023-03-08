5dd50a017441039bc5c5cf79fca87799a7189b92
Liudmyla <lyudmula1989@ukr.net>
2023-03-08 15:57:14 +0200

    Add api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import './App.css'
 import {Forecast} from "./components/forecast/Forecast";
 
 const App = () => {
     return (
         <div>
-            <div className="container"><Forecast/>
-                <div><a href="https://github.com/Kaniuk/weather-react">Open-source code,</a> by <a
+            <div className="container"><Forecast defaultCity='Paris'/>
+                <div className='aboutAuthor'><a href="https://github.com/Kaniuk/weather-react">Open-source code,</a> by <a
                     href="https://www.linkedin.com/in/liudmyla-kaniuk-65b29b21b/">Liudmyla Kaniuk</a></div>
             </div>
         </div>
--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,41 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 
-import {Search} from "../search/Search";
 import "./Forecast.css";
+import axios from "axios";
 
-function Forecast() {
-    return (
-        <div className="forecast">
-            <div className="search">
-                <Search/>
-            </div>
-            <div className='city'>
-                <h1>Kyiv</h1>
-                <img src="#" alt="weather" width='100'/>
-            </div>
-            <div className="content row">
+function Forecast({defaultCity}) {
+    const [weatherData, setWeatherData] = useState({ready:false});
 
-                <div className="cityInfo col-7">
+    function handleResponse(response) {
+        setWeatherData({
+            ready:true,
+            temperature: Math.round(response.data.temperature.current),
+            city: response.data.city,
+            condition: response.data.condition.description,
+            humidity: response.data.temperature.humidity,
+            wind: response.data.wind.speed,
+            time: response.data.time,
+            icon: response.data.condition.icon_url
+        });
 
-                    <div className="info">
-                        <div>Last updated: Friday 10:32</div>
-                        <div>Sunny</div>
-                        <div>Humidity: 80%</div>
-                        <div>Wind: 10 km/h</div>
+    }
+
+
+    if (weatherData.ready) {
+        return (
+            <div className="forecast">
+                <div className="search">
+                    <div className="row">
+                        <form>
+                            <input type="text" name="city" placeholder="Enter city" className="col-8" autoFocus="on"/>
+                            <input type="submit" value="Search" className="col-3 btn-primary"/>
+                        </form>
                     </div>
                 </div>
-                <div className='col-4'>
-                    <div className="temperature d-flex">
-                        <div>2</div>
-                        <a href="/proj/June-2022/sheCodes/weather-react/public">°C</a>
-                        /
-                        <a href="/proj/June-2022/sheCodes/weather-react/public">°F</a>
-                    </div>
+                <div className="city">
+                    <h1>{weatherData.city}</h1>
+                    <img src={weatherData.icon} alt="weather" width="100"/>
                 </div>
+                <div className="content row">
 
+                    <div className="cityInfo col-7">
+
+                        <div className="info">
+                            <div>Last updated: {weatherData.time}</div>
+                            <div>{weatherData.condition}</div>
+                            <div>Humidity: {weatherData.humidity}</div>
+                            <div>Wind: {weatherData.wind}km/h</div>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className="temperature d-flex">
+                            <div>{weatherData.temperature}</div>
+                            <a href="/proj/June-2022/sheCodes/weather-react/public">°C</a>
+                            /
+                            <a href="/proj/June-2022/sheCodes/weather-react/public">°F</a>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        const apiKey = 'd1f3fa1bbd2e0t6f3b4o3faabd9a8d79';
+        const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${defaultCity}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+
+        return 'Loading...';
+    }
 }
 
 export {Forecast};
